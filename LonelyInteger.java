d2f4bc39b8416bb47aff6e32cc8cc363af361337
Yash Singhal <yashgyy@gmail.com>
2022-01-13 13:55:02 +0530

    Add

---
+++ LonelyInteger.java
@@ -0,0 +1,34 @@
+#HackerRank Lonely Integer
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<Integer> a) {
+    // Write your code here
+    int x = 0;
+    for (int num:a){
+        x = x ^ num;
+    }
+    return x;
+
+    }
+
+}
